2b3b22764781d52e38f5ec951cf8caf4926ec686
Roadcrosser <roadcrosser0@gmail.com>
2020-07-31 21:59:47 +0800

    Update everything + Add a block

---
+++ src/main/java/stacksnsignals/SolderingStation.java
@@ -0,0 +1,28 @@
+package stacksnsignals;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class SolderingStation extends Block {
+
+    public SolderingStation(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(new LiteralText("do thing here"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+}
--- src/main/java/stacksnsignals/Stacks_n_Signals.java
+++ src/main/java/stacksnsignals/Stacks_n_Signals.java
@@ -2,6 +2,14 @@ package stacksnsignals;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,11 +22,14 @@ public class Stacks_n_Signals implements ModInitializer {
     public static final String MOD_ID = "stacks-n-signals";
     public static final String MOD_NAME = "Stacks n' Signals";
 
+    public static final Block SOLDERING_STATION = new SolderingStation(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
 
-        //TODO: Initializer
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "soldering_station"), SOLDERING_STATION);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "soldering_station"), new BlockItem(SOLDERING_STATION, new Item.Settings().group(ItemGroup.MISC)));
     }
 
     public static void log(Level level, String message){
